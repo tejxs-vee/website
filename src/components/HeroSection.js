188331536379bef3344c320f7d0443c7306f4c65
Veedhulur, Tejas <tveedhulur3@gatech.edu>
2021-10-29 11:38:34 -0400

    initial commit

---
+++ src/components/HeroSection.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../App.css';
+import { Button } from './Button';
+import './HeroSection.css';
+
+function HeroSection() {
+  return (
+    <div className='hero-container'>
+      <video src='/videos/video-1.mp4' autoPlay loop muted />
+      <h1>TEJAS VEEDHULUR</h1>
+      <p>UI / UX Designer</p>
+      
+      </div>
+  );
+}
+
+export default HeroSection;
